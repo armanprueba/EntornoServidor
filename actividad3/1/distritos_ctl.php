5cb8060aba2d9c0e8ad742ac3c535f2a0f9b803b
Arman <armkeyahm2@alu.edu.gva.es>
2025-10-22 17:48:28 +0200

    1.5

--- actividad3/1/distritos_ctl.php
+++ actividad3/1/distritos_ctl.php
@@ -1,6 +1,19 @@
-<?php 
-$datos_distritos = array ('Algirós' => 36390, 'Benicalap' => 47752, 'Benimaclet' => 28064, 'Camins al Grau'
-=> 65552, 'Campanar' => 39164, 'Ciutat Vella' => 27983, 'L\'Eixample' => 42672, 'Extramurs' => 48536,
-'Jesus' => 51941, 'L\'Olivereta' => 48686, 'Patraix' => 57798, 'El Pla del Real' => 30444, 'Poblats Marítims'
-=> 55486, 'Pobles del Nord' => 6744, 'Pobles de l\'Oest' => 14546, 'Pobles del Sud' => 21289, 'Quatre
-Carreres' => 74555, 'Rascanya' => 53566, 'La Saïdia' => 47013);
+<?php
+    if(isset($_GET["todos"])){
+        require_once("datos_distritos.php");
+        ?><ul><?php ?>
+        foreach($datos_distritos as $distrito => $habitantes){
+            ?><li><?php ?>
+        }
+    ?><ul><?php ?>
+    }
+    elseif(isset($_GET["distritos"])){ 
+       $distritos = explode(" ", $_GET["distritos"]);
+       $habitantes = $distritos[0];
+       $nom_distrito = $distritos[1];
+       
+       echo "El distrito " . $nom_distrito . " tiene " . $habitantes . " habitantes";
+
+    }else{
+        header('Location: index.html');
+    }
---
+++ actividad3/1/form_distritos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html> 
+<html lang="es"> 
+<head>
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Document</title> 
+</head> 
+<body> 
+    <form name="formulario" method="get" action="distritos_ctl.php">
+      <label for="lang">Lenguaje</label>
+      <select name="distritos" id="distritos">;
+        <?php
+            require "datos_distritos.php";
+            /* Si se hace  $distrito => $habitantes se coge también el nombre del distrito, 
+               con solo $distrito solo cogemos el número de habitantes del distrito*/
+            foreach($datos_distritos as $distrito => $habitantes){
+                /*if(strtolower($distrito) == "patraix"){*/
+                if($distrito == "Patraix"){
+                    ?><option value="<?php echo $habitantes . " " . $distrito?>" selected><?php echo $distrito ?> </option><?php
+                }
+                else {
+                ?><option value="<?php echo $habitantes . " " . $distrito?>"><?php echo $distrito ?> </option><?php
+                }
+            }
+        ?>
+      </select>
+      <input type="checkbox" name="todos" id="todos" value="todos" />
+      <input type="submit" value="Enviar" />
+</form>
+</body> 
+</form> 
+</html> 
+
